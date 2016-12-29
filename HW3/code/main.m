57d935c267f6e6db246c0779e92a506f09bf194a
dex <dexiongc@gmail.com>
2016-12-29 22:32:15 +0100

    hm3 matlab code

--- HW3/code/main.m
+++ HW3/code/main.m
@@ -1,5 +1,5 @@
 %% load data
-data = importdata('classification_data_HWK3/EMGaussian.data');
+train_data = importdata('classification_data_HWK3/EMGaussian.data');
 test_data = importdata('classification_data_HWK3/EMGaussian.test');
 
 [T, d] = size(test_data);
@@ -25,56 +25,71 @@ A = ones(K, K)/6;
 A(1:(K+1):end) = 1/2;
 
 %% Q2 Data smoothing
-log_alpha = forward(test_data, pi, A, mu, sigma);
-log_beta = backward(test_data, A, mu, sigma);
+log_alpha_init = forward(test_data, pi, A, mu, sigma);
+log_beta_init = backward(test_data, A, mu, sigma);
 
-smooth_states = log_alpha + log_beta;
-log_likelihood = logsumexp(smooth_states(1,:));
+marginal_init = log_alpha + log_beta;
+loglik_init = logsumexp(marginal_init(1,:));
  
-smooth_states = exp(smooth_states - log_likelihood);
+marginal_init = exp(marginal_init - loglik_init);
 
 % plot 
 figure
 
 for k = 1:K
     subplot(4,1,k);
-    plot(smooth_state(1:100, k));
+    plot(marginal_init(1:100, k));
 end
 
-% for t = 1:T-1
-%     log_gaussian = log(mvnpdf(repmat(test_data(t+1, :), K, 1), mu, sigma));
-% %         repmat(log_alpha(t, :), K, 1)
-% %         repmat((log_beta(t+1, :)+loggaussian)', 1, K)
-%     smooth_lag_one(:,:,t) = repmat(log_alpha(t, :), K, 1) ...
-%         + repmat(log_beta(t+1, :)'+log_gaussian, 1, K) + log(A) ...
-%         - log_likelihood;
-% end
-% smooth_lag_one = exp(smooth_lag_one);
-% A = sum(smooth_lag_one, 3);
-% A = A./ repmat(sum(A,1), K, 1);
-% 
-% for k = 1:K
-%     mu(k, :) = sum(test_data.*repmat(smooth_states(:, k), 1, d), 1) ...
-%         / sum(smooth_states(:, k));
-%     sigma_tilde = zeros(d,d);
-%     for t = 1:T
-%         centered_obs = test_data(t, :)-mu(k, :);
-%         aux = centered_obs'*centered_obs;
-%         sigma_tilde = sigma_tilde+ aux * smooth_states(t, k);
-%     end
-%     sigma(:,:,k) = sigma_tilde/sum(smooth_states(:, k));
-% end
 
-
-%% Q3 Parameters estimation using EM algorithm
+%% Q4,Q5: Parameters estimation using EM algorithm
 n_iters = 10;
-[new_pi, new_A, new_mu, new_sigma, loglik] = EM(data, pi, A, mu, sigma, n_iters);
+[new_pi, new_A, new_mu, new_sigma, loglik_list] = EM(train_data, pi, A, mu, sigma, n_iters);
 
 % plot log-likelihood
 figure
-plot(loglik)
+plot(loglik_list)
 
-%% Viterbi decoding algorithm
+%% Q6 Compute log-likelihoods
+
+
+%% Q8 Viterbi decoding algorithm
+states = viterbi(train_data, new_pi, new_A, new_mu, new_sigma);
+figure
+gscatter(train_data(:, 1), train_data(:, 2), states, [], [], 15);
+
+
+%% Q9: Compute marginal probability for the test data with parameters learned on the training data
+log_alpha_test = forward(test_data, new_pi, new_A, new_mu, new_sigma);
+log_beta_test = backward(test_data, new_A, new_mu, new_sigma);
+marginal_test = log_alpha_test + log_beta_test;
+loglik_test = logsumexp(marginal_test(1,:));
+
+marginal_test = exp(marginal_test - loglik_test);
+
+figure
+
+for k = 1:K
+    subplot(4,1,k);
+    plot(marginal_test(1:100, k));
+end
+
+%% Q10: Compute most likely state according to the marginal probability
+% Q11: Compute most likely sequence of states using viterbi algorithm
+
+% most likely state according to the marginal proba
+[~, marginal_states] = max(marginal_test, [], 2);
+% most likely state according to Viterbi
+viterbi_states = viterbi(test_data, new_pi, new_A, new_mu, new_sigma);
+
+figure
+subplot(2,1,1);
+plot(marginal_states(1:100));
+title('most likely state according to the marginal probability')
+subplot(2,1,2);
+plot(viterbi_states(1:100));
+title('most likely state according to Viterbi algorithm')
+
 
 
 
--- HW3/code/viterbi.m
+++ HW3/code/viterbi.m
@@ -1,6 +1,28 @@
-function [states] = viterbi(pi, A, mu, sigma, n_iters)
+function [states] = viterbi(observations, pi, A, mu, sigma)
 % Viterbi decoding algorithm
 
+[T, d] = size(observations);
+K = length(A);
+states = zeros(1, T);
+
+% forward recursion
+log_alpha = zeros(T, K);
+log_alpha(1, :) = log(pi) + log(mvnpdf(repmat(observations(1, :), K, 1), mu, sigma))';
+indices = zeros(T, K);
+
+for t = 2:T
+    [log_alpha(t, :), indices(t, :)] = max(repmat(log_alpha(t - 1, :), K, 1) + log(A), [], 2);
+    log_alpha(t, :) = log_alpha(t, :) + log(mvnpdf(repmat(observations(t, :), K, 1), mu, sigma))';
+end
+
+[~, k] = max(log_alpha(T,:));
+states(T) = k;
+
+% backward recursion
+
+for t = (T-1):-1:1
+    states(t) = indices(t+1, states(t+1));
+end
 
 
 end
